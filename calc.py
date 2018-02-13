4b8b7507f6cc079175cf318f1e6646c65b0ef94c
Uli <uli.hitzel@gmail.com>
2018-02-13 15:03:56 +0800

    changed

---
+++ calc.py
@@ -0,0 +1,29 @@
+import json
+import pylab
+import random
+import math
+
+seconds=[]
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
+
+def myround(x, base=50):
+    return int(base * round(float(x)/base))
+
+with open('uber1.json') as json_data:
+	d = json.load(json_data)
+
+	trip_num = 1
+
+	for trip in d["history"]:
+		rtime = trip["request_time"]
+		stime = trip["start_time"]
+		wait = stime - rtime
+		# print "Trip " + str(trip_num) + ": " +  str(wait) + " seconds"
+		# print stime, rtime
+		print str(trip_num) + "," + str(myround(wait))
+		trip_num = trip_num + 1
+		seconds.append(wait)
+
+print "Average waiting time: " +str(mean(seconds))
---
+++ get_uber_data.sh
@@ -0,0 +1,1 @@
+curl --header 'Content-Type: application/json' --header "Authorization: Bearer XYZ"  "https://api.uber.com/v1.2/history?limit=50"
